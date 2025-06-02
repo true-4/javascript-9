987f626934b9fe9dc2a08b01b843f0f93fcf6375
kuga <esixterb58@gmail.com>
2025-06-02 16:35:58 +0300

    feat: task-3/1

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ getThis() // ссылается на LE документа потому что �
 
 const arrThis = () => console.log(this)
 arrThis() // не имеет собственного this ссылается на LE родителя вернёт undefined
+
+// 3
+// 3.1 TO DOO
+const nums1 = [12, 23, 42, 45, 111]
+const res1 = nums1.map(num => ({
+  number: num,
+  digitsSum: String(num).split('').reduce((acc, curr) => acc + Number(curr), 0)
+})).reduce((acc, curr) => acc.digitsSum >= curr.digitsSum ? acc : curr).number
+
+console.log(res1)
+
+// 3.2
+const obj2 = [{a: 1}, {a: 1, b: 2}, {a: 1, b: 2, c: 3}]
+const res2 = obj2.map((it) => {
+  return it
+})
+console.log(res2)
