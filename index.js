0610cc9884c412a47bf573a5fd9b4da8c4476c27
kuga <esixterb58@gmail.com>
2025-06-02 12:23:12 +0300

    feat: task-2 use strict (module)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// NO 'use strict'
+// 'use strict' не нужен так как этот файл подключен через type="module"  
 
-console.log(this) // ссылается на LE документа
+console.log(this) // ссылается на LE документа вернёт undefined так как пытается вернуть хоть что то
 
 function getThis() {
   console.log(this)
 }
-getThis() // ссылается на LE документа потому что не находит собственный this
+getThis() // ссылается на LE документа потому что не находит собственный this вернёт undefined
 
 const arrThis = () => console.log(this)
-arrThis() // не имеет собственного this ссылается на LE родителя
+arrThis() // не имеет собственного this ссылается на LE родителя вернёт undefined
