3bc4124ea7b27e99466080610422cd22e62a16ed
kuga <esixterb58@gmail.com>
2025-06-16 15:27:00 +0300

    fix: task-3/1 again-2

--- index.js
+++ index.js
@@ -19,15 +19,7 @@ const nums1 = [12, 23, 42, 45, 111];
 const res1 = nums1.map(num => ({ // проходимся по элементам массива с помощью map для того чтобы возвращать на каждой итерации объект с полями: 
   number: num, // указываем "ключ" number и присваеваем ей значение num
   digitsSum: String(num).split('').reduce((acc, curr) => acc + Number(curr), 0) // указываем "ключ" приводим его значение к строке чтобы число стало итерируемым разбиваем на отдельные числа и складываем с помощбю reduce
-})).sort(function (a, b) { return a.digitsSum > b.digitsSum ? -1 : a.digitsSum < b.digitsSum ? 1 : 0
-  // if (a.digitsSum > b.digitsSum) {
-  //   return -1
-  // }
-  // if (a.digitsSum < b.digitsSum) {
-  //   return 1
-  // }
-  // return 0
-})
+})).sort(function (a, b) { return a.digitsSum > b.digitsSum ? -1 : a.digitsSum < b.digitsSum ? 1 : 0 })
 
 console.log(res1)
 
